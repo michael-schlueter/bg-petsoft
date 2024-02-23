8902734365c1ec6958a266304cc294da4d30e39a
Michael Schlueter <schluet0r4life@gmail.com>
2024-02-23 14:19:22 +0100

    render branding and stats components to dashboard

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,8 +1,13 @@
+import Branding from "@/components/branding";
+import Stats from "@/components/stats";
+
 export default function Page() {
   return (
     <main>
       <div className="flex items-center justify-between text-white py-8">
-        
+        <Branding />
+
+        <Stats />
       </div>
     </main>
   );
