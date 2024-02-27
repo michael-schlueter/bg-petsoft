3e4391baeff36f1a664ea38a39f8ccbd331ac867
Michael Schlueter <schluet0r4life@gmail.com>
2024-02-27 13:12:30 +0100

    update selected pet id with event handler

--- src/pet-context-provider.tsx
+++ src/pet-context-provider.tsx
@@ -6,24 +6,33 @@ import { Pet } from "./lib/types";
 type PetContextProviderProps = {
   children: React.ReactNode;
   data: Pet[];
-}
+};
 
 type TPetContext = {
   pets: Pet[];
-  selectedPetId: string | null
-}
+  selectedPetId: string | null;
+  handleChangeSelectPetId: (id: string) => void;
+};
 
 export const PetContext = createContext<TPetContext | null>(null);
 
-export default function PetContextProvider({ children, data }: PetContextProviderProps) {
+export default function PetContextProvider({
+  children,
+  data,
+}: PetContextProviderProps) {
   const [pets, setPets] = useState(data);
-  const [selectedPetId, setSelectedPetId] = useState(null);
+  const [selectedPetId, setSelectedPetId] = useState<string | null>(null);
+
+  function handleChangeSelectPetId(id: string) {
+    setSelectedPetId(id);
+  }
 
   return (
     <PetContext.Provider
       value={{
         pets,
         selectedPetId,
+        handleChangeSelectPetId,
       }}
     >
       {children}
