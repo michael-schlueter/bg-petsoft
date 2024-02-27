3b9ebb2d70c6620a9fc0d233c7ce259d42854920
Michael Schlueter <schluet0r4life@gmail.com>
2024-02-27 13:19:51 +0100

    add background-color for active/selected pet

--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -1,13 +1,24 @@
+"use client";
+
 import { usePetContext } from "@/lib/hooks";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 
 export default function PetList() {
-  const { pets } = usePetContext();
+  const { pets, handleChangeSelectPetId, selectedPetId } = usePetContext();
   return (
     <ul className="bg-white border-b border-light">
       {pets.map((pet) => (
         <li key={pet.id}>
-          <button className="flex items-center h-[70px] w-full cursor-pointer px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition">
+          <button
+            onClick={() => handleChangeSelectPetId(pet.id)}
+            className={cn(
+              "flex items-center h-[70px] w-full cursor-pointer px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition",
+              {
+                "bg-[#EFF1F2]": selectedPetId === pet.id,
+              }
+            )}
+          >
             <Image
               src={pet.imageUrl}
               alt="Pet image"
