14a5357c2b27a05ab4f74aae344b1b1287f05e6d
Michael Schlueter <schluet0r4life@gmail.com>
2024-03-06 14:16:02 +0100

    add loading indicator for adding pet

--- src/components/pet-form.tsx
+++ src/components/pet-form.tsx
@@ -6,6 +6,7 @@ import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { Textarea } from "./ui/textarea";
 import { addPet } from "@/app/actions/actions";
+import PetFormBtn from "./pet-form-btn";
 
 type PetFormProps = {
   actionType: "add" | "edit";
@@ -79,9 +80,7 @@ export default function PetForm({ actionType, onSubmission }: PetFormProps) {
         </div>
       </div>
 
-      <Button type="submit" className="mt-5 self-end">
-        {actionType === "add" ? "Add a new pet" : "Edit pet"}
-      </Button>
+      <PetFormBtn actionType={actionType} />
     </form>
   );
 }
