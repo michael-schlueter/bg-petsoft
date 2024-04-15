d24d9c5fdff5899a4413410dc70258db5264c546
Michael Schlueter <schluet0r4life@gmail.com>
2024-04-15 17:19:43 +0200

    add auth loading state

--- src/app/actions/actions.ts
+++ src/app/actions/actions.ts
@@ -11,6 +11,8 @@ import { Prisma } from "@prisma/client";
 
 // --- user actions ---
 export async function logIn(formData: unknown) {
+  await sleep(1000);
+
   if (!(formData instanceof FormData)) {
     return {
       message: "Invalid form data.",
@@ -25,6 +27,8 @@ export async function logOut() {
 }
 
 export async function signUp(formData: unknown) {
+  await sleep(1000);
+
   // check if formData is a FormData type
   if (!(formData instanceof FormData)) {
     return {
---
+++ src/components/ui/auth-btn.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+import { Button } from "./button";
+
+type AuthBtnProps = {
+  type: "signUp" | "logIn";
+};
+
+export default function AuthBtn({ type }: AuthBtnProps) {
+  const { pending } = useFormStatus();
+  return (
+    <Button disabled={pending}>
+      {type === "signUp" ? "Sign Up" : "Log In"}
+    </Button>
+  );
+}
